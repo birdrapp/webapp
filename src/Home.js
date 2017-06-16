c43a6e25db062bb7efca7f55d8f495dee1e50fed
Robin Murphy <robin@robinmurphy.co.uk>
2017-06-16 21:12:44 +0100

    Add link to homepage buttons

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Grid } from 'semantic-ui-react';
 import './Home.css';
 import logo from './logo.svg';
+import { Link } from 'react-router-dom';
 
 export default () => {
   return (
@@ -13,12 +14,20 @@ export default () => {
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
-          <Grid.Column width={0} computer={5} />
-          <Grid.Column width={16} computer={6}>
-            <Button primary fluid style={{ marginBottom: '8px' }}>
+          <Grid.Column computer="5" />
+          <Grid.Column width="16" computer="6">
+            <Button
+              primary
+              fluid
+              style={{ marginBottom: '8px' }}
+              as={Link}
+              to="/sign-up"
+            >
               Create an account
             </Button>
-            <Button fluid>Already a member? Sign in</Button>
+            <Button fluid as={Link} to="sign-in">
+              Already a member? Sign in
+            </Button>
           </Grid.Column>
         </Grid.Row>
       </Grid>
