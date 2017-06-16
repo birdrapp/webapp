3b28517dab3ab475b86992505ff5eb586080c713
Robin Murphy <robin@robinmurphy.co.uk>
2017-06-16 20:20:13 +0100

    Add router and some basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import Home from './Home';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import Profile from './Profile';
+import EditProfile from './EditProfile';
+import PasswordReset from './PasswordReset';
+import NotFound from './NotFound';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <Router>
+        <div>
+          <Container>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/sign-in" component={SignIn} />
+              <Route path="/sign-up" component={SignUp} />
+              <Route path="/profile" component={Profile} />
+              <Route path="/profile/edit" component={EditProfile} />
+              <Route path="/password-reset" component={PasswordReset} />
+              <Route component={NotFound} />
+            </Switch>
+          </Container>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/PasswordReset.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => {
+  return <h1>Password reset</h1>;
+};
